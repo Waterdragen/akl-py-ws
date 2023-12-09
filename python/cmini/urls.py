b810f00ffbb3924a7a14e57c439357000e580de1
James Yip <waterdragenarchive@gmail.com>
2023-12-09 20:16:20 +0800

    Add bad request handler

--- python/cmini/urls.py
+++ python/cmini/urls.py
@@ -7,5 +7,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("commands/", views.send_cmini_response)
 ]
--- python/cmini/views.py
+++ python/cmini/views.py
@@ -5,10 +5,5 @@ from django.shortcuts import render
 from django.http.request import HttpRequest
 from django.http import HttpResponse
 
-from .src.main import get_cmini_response
-
-def send_cmini_response(request: HttpRequest):
-    default_command = "!cmini help"
-    command: str = request.POST.get("command", default_command)
-    response: str = get_cmini_response(command)
-    return HttpResponse(response)
+def bad_request(request: HttpRequest):
+    return render(request, "bad_request.html", status=400)
