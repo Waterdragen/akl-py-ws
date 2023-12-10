f75dc6ccc7e6392fe7e0db0ea543801e1256b430
James Yip <waterdragenarchive@gmail.com>
2023-12-10 21:52:54 +0800

    Add rust websocket

--- python/backend/settings.py
+++ python/backend/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR.parent, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
