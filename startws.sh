547d31aff2facbacc9c0f5f576781ee8b536197b
James Yip <waterdragenarchive@gmail.com>
2024-01-14 13:10:46 +0800

    Update startws.sh

--- startws.sh
+++ startws.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
+
+# Start Actix server for oxeylyzer
+cargo run --release --manifest-path rust/Cargo.toml &
  
 # Start Django server for cmini
 python3 python/manage.py runserver 127.0.0.1:9000 &
- 
-# Start Actix server for oxeylyzer
-cargo run --release --manifest-path rust/Cargo.toml &
  
 nginx -g 'daemon off;' &
 
