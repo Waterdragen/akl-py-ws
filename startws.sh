e00130f778b75d973a64408425e6fcea3dcf2677
James Yip <waterdragenarchive@gmail.com>
2024-01-14 15:57:38 +0800

    Update shell

--- startws.sh
+++ startws.sh
@@ -1,9 +1,7 @@
 #!/bin/bash 
 
 # Start Actix server for oxeylyzer
-cd ./rust &&
-cargo run --release &
-cd ../ &&
+cargo run --manifest-path ./rust/Cargo.toml &
  
 # Start Django server for cmini
 python3 python/manage.py runserver 127.0.0.1:9000 &
