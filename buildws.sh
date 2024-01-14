d4f74510ac87852e5dc894e9bf39bd248c150780
James Yip <waterdragenarchive@gmail.com>
2024-01-14 17:14:12 +0800

    Update shell

--- buildws.sh
+++ buildws.sh
@@ -6,9 +6,3 @@ pip3 install --no-cache-dir -r requirements.txt &&
 # Install rust dependencies
 cargo build --release --manifest-path ./rust/Cargo.toml ;
 
-directory="./rust/target/release"
-
-if [ ! -d "$directory" ]; then
-    echo "App deployment failed: wrong rust build directory"
-    exit 1
-fi
--- startws.sh
+++ startws.sh
@@ -1,22 +1,13 @@
 #!/bin/bash 
 
-directory="./rust/target/release"
-
-if [ -d "$directory" ]; then
-
 # Start Actix server for oxeylyzer
 cargo run --release --manifest-path ./rust/Cargo.toml &
  
 # Start Django server for cmini
-python3 python/manage.py runserver 127.0.0.1:9000 &
+python3 ./python/manage.py runserver 127.0.0.1:9000 &
  
 nginx -g 'daemon off;' &
 
 echo "App is running" &
 
 wait
-
-else
-    echo "App deployment failed: rust will recompile"
-    exit 1
-fi
