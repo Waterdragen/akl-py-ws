6f1543161dba32f091e4d83d8bb054ff9f3c2abf
James Yip <waterdragenarchive@gmail.com>
2024-01-14 14:44:56 +0800

    Update buildws.sh

--- buildws.sh
+++ buildws.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
  
 # Install python dependencies
-pip3 install --no-cache-dir -r requirements.txt &
+pip3 install --no-cache-dir -r requirements.txt &&
  
 # Install rust dependencies
-cargo build --release --manifest-path rust/Cargo.toml 
+cargo build --release --manifest-path rust/Cargo.toml ;
