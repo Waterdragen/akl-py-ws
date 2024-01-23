5d88d65f3d317a6aefdd4491a2c28016335cbf77
James Yip <waterdragenarchive@gmail.com>
2024-01-23 22:26:23 +0800

    Update shell

--- buildws.sh
+++ buildws.sh
@@ -1,10 +1,7 @@
 #!/bin/bash 
-
-rustup install stable &&
-rustup default stable &&
  
 # Install python dependencies
 pip3 install --no-cache-dir -r requirements.txt &&
  
 # Install rust dependencies
-cargo build --release --manifest-path ./rust/Cargo.toml ;
+rustup run stable cargo build --release --manifest-path ./rust/Cargo.toml ;
