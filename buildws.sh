76dfdaf61fd4095bd243c9ad4d56b80bdea28a06
Waterdragen <96000090+Waterdragen@users.noreply.github.com>
2024-03-03 18:13:11 +0800

    Update buildws.sh

--- buildws.sh
+++ buildws.sh
@@ -2,9 +2,9 @@
  
 # Install python dependencies
 pip3 install --no-cache-dir -r requirements.txt &&
- 
-# Install rust dependencies
-(cd ./rust && cargo build --release) &&
 
 # Install go dependencies
-(cd ./go && go get ./... && go build)
+(cd ./go && go get ./... && go build) &&
+ 
+# Install rust dependencies
+(cd ./rust && cargo build --release)
