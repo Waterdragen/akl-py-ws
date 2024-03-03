66d0d48d89a83b9f84468608aaacc707bad42f50
James Yip <waterdragenarchive@gmail.com>
2024-03-03 17:28:42 +0800

    Update shell to install go dependencies

--- buildws.sh
+++ buildws.sh
@@ -4,4 +4,7 @@
 pip3 install --no-cache-dir -r requirements.txt &&
  
 # Install rust dependencies
-(cd ./rust && cargo build --release)
+(cd ./rust && cargo build --release) &&
+
+# Install go dependencies
+(cd ./go && go get ./...)
--- startws.sh
+++ startws.sh
@@ -7,7 +7,7 @@
 python3 ./python/manage.py runserver 127.0.0.1:9000 &
 
 # Test Go
-(cd ./go && go run main.go) &&
+(cd ./go && go run main.go) &
  
 nginx -g 'daemon off;' &
 
